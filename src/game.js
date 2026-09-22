709af812ac8e10317d61218e79ffcc9a2fe30eb8
okangoksel <okangoksel5@gmail.com>
2026-09-22 23:59:24 +0300

    Add guidebook shortcut

--- src/game.js
+++ src/game.js
@@ -19,7 +19,7 @@ export class Game{
   if(k==="f3"){this.debug=!this.debug;this.ui.updateDebug();return}
   if(k==="escape"){this.paused=!this.paused;this.ui.setMenu(this.paused);return}
   if(k==="i"){this.ui.toggleInventory();return}
-  if(k==="c"){this.ui.toggleCrafting();return}if(k==="m"){this.ui.toggleMap();return}if(k==="j"){this.ui.toggleQuests();return}if(k==="p"){this.ui.toggleMarketplace();return}if(k==="k"){this.ui.toggleDiscoveries();return}
+  if(k==="c"){this.ui.toggleCrafting();return}if(k==="m"){this.ui.toggleMap();return}if(k==="j"){this.ui.toggleQuests();return}if(k==="p"){this.ui.toggleMarketplace();return}if(k==="k"){this.ui.toggleDiscoveries();return}if(k==="l"){this.ui.toggleGuide();return}
   if(k>="1"&&k<="9")this.player.hotbar=Number(k)-1;
   if(k==="e")this.systems.interact();
  }
