162063276ff8292a4b95b1d73930010cb3062679
okangoksel <okangoksel5@gmail.com>
2026-09-23 23:04:36 +0300

    v0.4.3 cache bust

--- src/game.js
+++ src/game.js
@@ -1,10 +1,10 @@
-import {Player} from "./player.js?v=0.4.1";
-import {World} from "./world.js?v=0.4.1";
-import {Systems} from "./systems.js?v=0.4.1";
-import {UI} from "./ui.js?v=0.4.1";
-import {QuestSystem} from "./quests.js?v=0.4.1";
+import {Player} from "./player.js?v=0.4.3";
+import {World} from "./world.js?v=0.4.3";
+import {Systems} from "./systems.js?v=0.4.3";
+import {UI} from "./ui.js?v=0.4.3";
+import {QuestSystem} from "./quests.js?v=0.4.3";
 
-export const GAME_VERSION="0.4.1";
+export const GAME_VERSION="0.4.3";
 
 export class Game{
  constructor(canvas){
