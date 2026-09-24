1d31818f1e94e78c144ae1c017eb283538e4a716
okangoksel <okangoksel5@gmail.com>
2026-09-24 22:56:14 +0300

    Bump main cache version for NPC fix

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ if(canvas){
 
 (async()=>{
   try{
-    const mod=await import("./game.js?v=0.4.1");
+    const mod=await import("./game.js?v=0.4.6");
     const Game=mod.Game;
     const game=new Game(canvas);
     window.starRepublic=game;
