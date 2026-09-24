4e65c6a3c899583b52323c2d3576e54033730737
okangoksel <okangoksel5@gmail.com>
2026-09-24 22:51:38 +0300

    Bump game cache version for tutorial fix

--- src/game.js
+++ src/game.js
@@ -1,10 +1,10 @@
 import {Player} from "./player.js?v=0.4.4";
 import {World} from "./world.js?v=0.4.4";
 import {Systems} from "./systems.js?v=0.4.4";
-import {UI} from "./ui.js?v=0.4.4";
+import {UI} from "./ui.js?v=0.4.5";
 import {QuestSystem} from "./quests.js?v=0.4.4";
 
-export const GAME_VERSION="0.4.4";
+export const GAME_VERSION="0.4.5";
 
 export class Game{
  constructor(canvas){
