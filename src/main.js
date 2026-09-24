29febe10264b7148e28f1fd68066894ca1299a3a
okangoksel <okangoksel5@gmail.com>
2026-09-24 22:59:54 +0300

    Update main cache version

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ if(canvas){
 
 (async()=>{
   try{
-    const mod=await import("./game.js?v=0.4.6");
+    const mod=await import("./game.js?v=0.4.7");
     const Game=mod.Game;
     const game=new Game(canvas);
     window.starRepublic=game;
