ca64dfd329b42b741b7ea6fac096237959efe26c
okangoksel <okangoksel5@gmail.com>
2026-09-24 22:59:49 +0300

    Bump version after syntax fix

--- src/game.js
+++ src/game.js
@@ -1,10 +1,10 @@
-import {Player} from "./player.js?v=0.4.6";
-import {World} from "./world.js?v=0.4.6";
-import {Systems} from "./systems.js?v=0.4.6";
+import {Player} from "./player.js?v=0.4.7";
+import {World} from "./world.js?v=0.4.7";
+import {Systems} from "./systems.js?v=0.4.7";
 import {UI} from "./ui.js?v=0.4.5";
-import {QuestSystem} from "./quests.js?v=0.4.6";
+import {QuestSystem} from "./quests.js?v=0.4.7";
 
-export const GAME_VERSION="0.4.6";
+export const GAME_VERSION="0.4.7";
 
 export class Game{
  constructor(canvas){
